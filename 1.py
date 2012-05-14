8f1fc07bfe6fcb1cc1949b824edd1072ee655367
senft <senftt@gmail.com>
2012-05-14 14:38:35 +0200

    Speeded up 14, tweaked 1, added 15 & 16

--- 1.py
+++ 1.py
@@ -1,2 +1,2 @@
 # Correct answer: 233168
-sum([x for x in xrange(1000) if x % 3 == 0 or x % 5 == 0])
+print sum([x for x in xrange(1000) if x % 3 == 0 or x % 5 == 0])
--- 14.py
+++ 14.py
@@ -10,15 +10,23 @@ MAX = 1000000
 
 @memoize
 def len_sequence(n):
-    if n == 1:
-        return 1
+    if n < 2:
+        return n
 
     return 1 + len_sequence(n / 2) if n % 2 == 0 else \
             1 + len_sequence(3 * n + 1)
 
 
 def main():
-    print max([(len_sequence(n), n) for n in xrange(1, MAX)])
+    max_len, max_start = (0, 0)
+
+    for n in xrange(1, MAX):
+        new_len, new_start = len_sequence(n), n
+        if new_len > max_len:
+            max_start = new_start
+            max_len = new_len
+
+    print max_start, max_len
 
 if __name__ == '__main__':
     main()
